83d2dcd44821111123604009b4d4073edbb299f5
Ruslan Yanchyshyn <ruslan.yanchyshyn@lemberg.co.uk>
2017-12-11 13:56:34 +0200

    Minor changes.

--- MotionGesturesDemo/app/src/main/java/uk/co/lemberg/motiongesturesdemo/MainActivity.java
+++ MotionGesturesDemo/app/src/main/java/uk/co/lemberg/motiongesturesdemo/MainActivity.java
@@ -1,11 +1,19 @@
 package uk.co.lemberg.motiongesturesdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 import uk.co.lemberg.motiondetectionlib.MotionDetector;
 
@@ -13,17 +21,38 @@ public class MainActivity extends AppCompatActivity {
 
 	private static final String TAG = MainActivity.class.getSimpleName();
 
+	private ScrollView scrollLogs;
+	private TextView textLogs;
+
 	private MotionDetector motionDetector;
+	private DateFormat dateFormat;
+	private DateFormat timeFormat;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		motionDetector = new MotionDetector(this, gestureListener);
+		scrollLogs = findViewById(R.id.scrollLogs);
+		textLogs = findViewById(R.id.textLogs);
 
-		ToggleButton toggleRec = findViewById(R.id.toggleRec);
-		toggleRec.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+		motionDetector = new MotionDetector(this, gestureListener);
+	}
+
+	@Override
+	protected void onDestroy() {
+		motionDetector.stop();
+		super.onDestroy();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+
+		MenuItem switchItem = menu.findItem(R.id.actionSwitch);
+
+		SwitchCompat switchActionBar = switchItem.getActionView().findViewById(R.id.switchActionBar);
+		switchActionBar.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 			@Override
 			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 				if (isChecked) {
@@ -40,15 +69,39 @@ public class MainActivity extends AppCompatActivity {
 				}
 			}
 		});
+
+		return true;
 	}
 
 	private final MotionDetector.Listener gestureListener = new MotionDetector.Listener() {
 		@Override
 		public void onGestureRecognized(MotionDetector.GestureType gestureType) {
 			showToast(gestureType.toString());
+			addLog("Gesture detected: " + gestureType);
 			Log.d(TAG, "Gesture detected: " + gestureType);
 		}
 	};
+
+	private DateFormat getDateFormat() {
+		if (dateFormat == null) {
+			dateFormat = android.text.format.DateFormat.getDateFormat(this);
+		}
+		return dateFormat;
+	}
+
+	private DateFormat getTimeFormat() {
+		if (timeFormat == null) {
+			timeFormat = android.text.format.DateFormat.getTimeFormat(this);
+		}
+		return timeFormat;
+	}
+
+	private void addLog(String str) {
+		Date date = new Date();
+		String logStr = String.format("[%s %s] %s\n", getDateFormat().format(date), getTimeFormat().format(date), str);
+		textLogs.append(logStr);
+		scrollLogs.fullScroll(View.FOCUS_DOWN);
+	}
 
 	private void showToast(String str) {
 		Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
